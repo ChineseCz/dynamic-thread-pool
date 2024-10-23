eba7f60b22f9c91ebd21273f6321e32a9a6ea3ff
weixin_45845008 <weixin_45845008@noreply.gitcode.com>
2024-10-23 15:18:11 +0800

    service分支

--- dynamic-thread-pool-spring-boot-starter/src/main/java/com/chinesecz/middleware/dynamic/thread/pool/sdk/config/DynamicThreadPoolAutoConfig.java
+++ dynamic-thread-pool-spring-boot-starter/src/main/java/com/chinesecz/middleware/dynamic/thread/pool/sdk/config/DynamicThreadPoolAutoConfig.java
@@ -3,12 +3,23 @@ package com.chinesecz.middleware.dynamic.thread.pool.sdk.config;
 
 import com.alibaba.fastjson.JSON;
 import com.chinesecz.middleware.dynamic.thread.pool.sdk.domain.DynamicThreadPoolService;
+import com.chinesecz.middleware.dynamic.thread.pool.sdk.domain.IDynamicThreadPoolService;
+import com.chinesecz.middleware.dynamic.thread.pool.sdk.registry.IRegistry;
+import com.chinesecz.middleware.dynamic.thread.pool.sdk.registry.redis.RedisRegistry;
+import com.chinesecz.middleware.dynamic.thread.pool.sdk.trigger.job.ThreadPoolDataReportJob;
 import org.apache.commons.lang.StringUtils;
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.codec.JsonJacksonCodec;
+import org.redisson.config.Config;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 import java.util.Map;
 import java.util.Set;
@@ -20,8 +31,40 @@ import java.util.concurrent.ThreadPoolExecutor;
     动态配置入口
  */
 @Configuration
+@EnableConfigurationProperties(DynamicThreadPoolAutoProperties.class)
+@EnableScheduling
 public class DynamicThreadPoolAutoConfig {
     private final Logger logger = LoggerFactory.getLogger(DynamicThreadPoolAutoConfig.class);
+    @Bean("dynamicThreadRedissonClient")
+    public RedissonClient redissonClient(DynamicThreadPoolAutoProperties properties) {
+        Config config = new Config();
+        // 根据需要可以设定编解码器；https://github.com/redisson/redisson/wiki/4.-%E6%95%B0%E6%8D%AE%E5%BA%8F%E5%88%97%E5%8C%96
+        config.setCodec(JsonJacksonCodec.INSTANCE);
+
+        config.useSingleServer()
+                .setAddress("redis://" + properties.getHost() + ":" + properties.getPort())
+                .setPassword(properties.getPassword())
+                .setConnectionPoolSize(properties.getPoolSize())
+                .setConnectionMinimumIdleSize(properties.getMinIdleSize())
+                .setIdleConnectionTimeout(properties.getIdleTimeout())
+                .setConnectTimeout(properties.getConnectTimeout())
+                .setRetryAttempts(properties.getRetryAttempts())
+                .setRetryInterval(properties.getRetryInterval())
+                .setPingConnectionInterval(properties.getPingInterval())
+                .setKeepAlive(properties.isKeepAlive())
+        ;
+
+        RedissonClient redissonClient = Redisson.create(config);
+
+        logger.info("动态线程池，注册器（redis）链接初始化完成。{} {} {}", properties.getHost(), properties.getPoolSize(), !redissonClient.isShutdown());
+
+        return redissonClient;
+    }
+
+    @Bean
+    public IRegistry redisRegistry(RedissonClient dynamicThreadRedissonClient) {
+        return new RedisRegistry(dynamicThreadRedissonClient);
+    }
 
     @Bean("dynamicThreadPoolService")
     public DynamicThreadPoolService dynamicThreadPoolService(ApplicationContext applicationContext, Map<String,ThreadPoolExecutor> threadPoolExecutorMap) {
@@ -30,19 +73,14 @@ public class DynamicThreadPoolAutoConfig {
         if (StringUtils.isBlank(applicationName)) {
             logger.warn("无应用名称");
         }
-//
-//        Set<String> threadPoolKeys = threadPoolExecutorMap.keySet();
-//        for (String threadPoolKey:threadPoolKeys) {
-//            ThreadPoolExecutor threadPoolExecutor = threadPoolExecutorMap.get(threadPoolKeys);
-//            int poolSize = threadPoolExecutor.getPoolSize();
-//            int corePoolSize = threadPoolExecutor.getCorePoolSize();
-//            BlockingQueue<Runnable> queue = threadPoolExecutor.getQueue();
-//            String simpleName = queue.getClass().getSimpleName();
-//
-//        }
 
         logger.info("线程池信息：{}", JSON.toJSONString(threadPoolExecutorMap.keySet()));
         return new DynamicThreadPoolService(applicationName,threadPoolExecutorMap );
+    }
+
+    @Bean
+    public ThreadPoolDataReportJob threadPoolDataReportJob(IDynamicThreadPoolService dynamicThreadPoolService, IRegistry registry) {
+        return new ThreadPoolDataReportJob(dynamicThreadPoolService, registry);
     }
 
 }
